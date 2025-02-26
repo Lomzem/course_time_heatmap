503838920817cab5985a3bb53f07d074d8a29d7d
Lawjay Lee <lawjaylee1@icloud.com>
2025-02-25 20:36:32 -0800

    got pydantic model to work

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import json
+from models import Model
 
 
 def main():
     with open("out.json") as f:
         data = json.load(f)
 
-    print(data)
+    for course in data["classes"]:
+        # print(course, "\n\n")
+        course = Model(**course)
+        print(course)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -5,88 +5,69 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
-class Instructor(BaseModel):
-    name: str
-    email: str
-
-
 class Meeting(BaseModel):
     days: str
     start_time: str
     end_time: str
     start_dt: str
     end_dt: str
-    bldg_cd: Optional[str] = None
-    bldg_has_coordinates: Optional[bool] = None
-    facility_descr: str
-    room: Optional[str] = None
-    facility_id: str
-    instructor: str
+    # bldg_cd: str
+    # bldg_has_coordinates: bool
+    # facility_descr: str
+    # room: str
+    # facility_id: str
+    # instructor: str
 
 
-class ReservedCombinedInfo(BaseModel):
-    class_enrl_cap: int
-    combined_enrl_cap: int
-    enrl_tot_other_combined: int
-
-
-class Class(BaseModel):
-    index: int
-    crse_id: str
-    crse_offer_nbr: int
-    strm: str
-    session_code: str
-    session_descr: str
-    class_section: str
-    location: str
-    location_descr: str
+class Model(BaseModel):
+    # index: int
+    # crse_id: str
+    # crse_offer_nbr: int
+    # strm: str
+    # session_code: str
+    # session_descr: str
+    # class_section: str
+    # location: str
+    # location_descr: str
     start_dt: str
     end_dt: str
-    class_stat: str
-    campus: str
-    campus_descr: str
-    class_nbr: int
-    acad_career: str
-    acad_career_descr: str
-    component: str
+    # class_stat: str
+    # campus: str
+    # campus_descr: str
+    # class_nbr: int
+    # acad_career: str
+    # acad_career_descr: str
+    # component: str
     subject: str
     subject_descr: str
     catalog_nbr: str
     class_type: str
-    schedule_print: str
-    acad_group: str
+    # schedule_print: str
+    # acad_group: str
     instruction_mode: str
     instruction_mode_descr: str
-    acad_org: str
-    grading_basis: str
-    wait_tot: int
-    wait_cap: int
-    class_capacity: int
-    enrollment_total: int
-    enrollment_available: int
-    descr: str
-    rqmnt_designtn: str
-    units: str
-    combined_section: str
-    enrl_stat: str
-    enrl_stat_descr: str
-    topic: str
-    instructors: List[Instructor]
-    section_type: str
-    meetings: List[Meeting]
-    crse_attr: str
-    crse_attr_value: str
-    reserve_caps: List
-    isInCart: bool
-    isEnrolled: bool
-    isWaitlisted: bool
-    notes: List[str]
-    icons: List
-    combined_descr: Optional[str] = None
-    reserved_combined_info: Optional[ReservedCombinedInfo] = None
-
-
-class Model(BaseModel):
-    pageCount: int
-    classes: List[Class]
+    # acad_org: str
+    # grading_basis: str
+    # wait_tot: int
+    # wait_cap: int
+    # class_capacity: int
+    # enrollment_total: int
+    # enrollment_available: int
+    # descr: str
+    # rqmnt_designtn: str
+    # units: str
+    # combined_section: str
+    # enrl_stat: str
+    # enrl_stat_descr: str
+    # topic: str
+    # section_type: str
+    meetings: Optional[List[Meeting]]
+    # crse_attr: str
+    # crse_attr_value: str
+    # reserve_caps: List
+    # isInCart: bool
+    # isEnrolled: bool
+    # isWaitlisted: bool
+    # notes: List
+    # icons: List
 
