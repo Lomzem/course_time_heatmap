d4f990291836d38077bfe0deecd98829ee53b952
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-19 23:56:39 -0700

    Antoher

--- supabase.py
+++ supabase.py
@@ -1,8 +1,6 @@
-import datetime
 import json
 import logging
 import os
-import random
 
 import pandas as pd
 import sqlalchemy
@@ -12,10 +10,22 @@ from sqlalchemy.exc import IntegrityError
 
 import supabase_models as models
 from models import Model
-from supabase_models import instructionModeEnum
 
 
-def add_weekdays(session: sqlalchemy.orm.Session):
+def main():
+    logging.basicConfig(level=logging.INFO)
+    load_dotenv(".env")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if not DATABASE_URL:
+        raise Exception("DATABASE_URL is not set")
+    pass
+
+    engine = sqlalchemy.create_engine(DATABASE_URL)
+    SessionLocal = sqlalchemy.orm.sessionmaker(bind=engine)
+    Base = sqlalchemy.orm.declarative_base()
+
+    session = SessionLocal()
+
     weekdays = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday"}
     db_weekdays = [row[0] for row in session.query(models.Weekday.name).all()]
 
@@ -35,16 +45,17 @@ def add_weekdays(session: sqlalchemy.orm.Session):
             logging.error(f"Error adding weekday {id} {name}: {e}")
             session.rollback()
 
-
-def add_timeslots(session: sqlalchemy.orm.Session):
     start_time = pd.Timestamp("07:00:00")
     end_time = pd.Timestamp("18:00:00")
     minute_index = pd.date_range(start_time, end_time, freq="1min").time
 
-    db_timeslots = [row[0] for row in session.query(models.TimeSlot.time).all()]
+    # db_timeslots = [row[0] for row in session.query(models.TimeSlot.time).all()]
+    db_timeslots = session.query(models.TimeSlot).all()
+    timeslots_map = {timeslot.time: timeslot.id for timeslot in db_timeslots}
+    # occupancy_map = {f}
 
     for id, minute in enumerate(minute_index):
-        if minute in db_timeslots:
+        if minute in timeslots_map.keys():
             logging.info(f"Timeslot(id:{minute}) already exists. Skipping.")
             continue
         try:
@@ -58,24 +69,6 @@ def add_timeslots(session: sqlalchemy.orm.Session):
         except Exception as e:
             logging.error(f"Error adding timeslot {minute}: {e}")
             session.rollback()
-
-
-def main():
-    logging.basicConfig(level=logging.INFO)
-    load_dotenv(".env")
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    if not DATABASE_URL:
-        raise Exception("DATABASE_URL is not set")
-    pass
-
-    engine = sqlalchemy.create_engine(DATABASE_URL)
-    SessionLocal = sqlalchemy.orm.sessionmaker(bind=engine)
-    Base = sqlalchemy.orm.declarative_base()
-
-    session = SessionLocal()
-
-    add_weekdays(session)
-    add_timeslots(session)
 
     with open("./data.json", "r") as f:
         data = json.load(f)
@@ -93,6 +86,11 @@ def main():
         f"{s.courseId}{s.termId}{s.classSection}": s.id for s in db_sessions
     }
 
+    db_occupancies = session.query(models.Occupancy).all()
+    occupancy_map = {
+        f"{o.sessionId}{o.weekdayId}{o.timeSlotId}": o.id for o in db_occupancies
+    }
+
     for course in data["classes"]:
         course = Model(**course)
         courses.append(course)
@@ -101,7 +99,7 @@ def main():
         if course.subject not in db_major_names:
             try:
                 new_major = models.Major(
-                    id=random.randint(1, 2**16 - 1), name=course.subject
+                    name=course.subject
                 )
                 session.add(new_major)
                 session.commit()
@@ -125,35 +123,93 @@ def main():
                 )
                 db_course_ids.append(course.crse_id)
             except IntegrityError:
-                logging.info(f"Course(id:{course.crse_id}) already exists. Skipping.")
+                logging.info(
+                    f"Course(id:{course.crse_id}, termId:{course.strm}, classSection:{course.class_section}) already exists. Skipping."
+                )
                 session.rollback()
 
         # Insert Session
-        if f"{course.crse_id}{course.strm}{course.class_section}" not in sessions_map:
+        if (
+            f"{int(course.crse_id)}{int(course.strm)}{int(course.class_section)}"
+            not in sessions_map.keys()
+        ):
             instruction_mode = (
-                instructionModeEnum.inperson
+                "inperson"
                 if course.instruction_mode.lower() == "p"
-                else instructionModeEnum.virtual
+                else "virtual"
             )
 
             new_session = models.Session(
-                id=random.randint(1, 2**16 - 1),
                 courseId=course.crse_id,
                 termId=course.strm,
                 classSection=course.class_section,
-                instructionMode=instruction_mode.value,
+                instructionMode=instruction_mode,
             )
 
             try:
                 session.add(new_session)
                 session.commit()
                 logging.info(
-                    f"Added Session(id:{new_session.id}, courseId:{course.crse_id}, termId:{course.strm}, classSection:{course.class_section}, instructionMode:{instruction_mode.value})"
+                    f"Added Session(id:{new_session.id}, courseId:{course.crse_id}, termId:{course.strm}, classSection:{course.class_section}, instructionMode:{instruction_mode})"
                 )
-                sessions_map[f"{course.crse_id}{course.strm}{course.class_section}"] = new_session.id
+                sessions_map[f"{course.crse_id}{course.strm}{course.class_section}"] = (
+                    new_session.id
+                )
             except IntegrityError:
                 logging.info(f"Session(id:{new_session.id}) already exists. Skipping.")
                 session.rollback()
+        else:
+            logging.info(f"Session(courseId:{course.crse_id}, termId:{course.strm}, classSection:{course.class_section}) already exists. Skipping.")
+
+        # Insert Occupancy
+                # f"{o.sessionId}{o.weekdayId}{o.timeSlotId}": o.id for o in db_occupancies
+
+        weekdays = {"Mo": 1, "Tu": 2, "We": 3, "Th": 4, "Fr": 5}
+        includes_weekdays = set()
+        for day in weekdays.keys():
+            if not course.meetings:
+                continue
+            if day in course.meetings[0].days:
+                includes_weekdays.add(day)
+
+        for time in timeslots_map.keys():
+            if not course.meetings:
+                continue
+            if not time:
+                continue
+
+            session_id = sessions_map[f"{int(course.crse_id)}{int(course.strm)}{int(course.class_section)}"]
+            time_slot_id = timeslots_map[time]
+
+            if start_time.time() <= time <= end_time.time():
+                for day in weekdays:
+                    weekday_Id = weekdays[day]
+
+                    if (f"{session_id}{weekday_Id}{time_slot_id}") not in occupancy_map.keys():
+                        new_occupancy = models.Occupancy(
+                            sessionId=session_id,
+                            weekdayId=weekday_Id,
+                            timeSlotId=time_slot_id,
+                            studentCount=course.enrollment_total,
+                        )
+                        try:
+                            session.add(new_occupancy)
+                            session.commit()
+                            logging.info(
+                                f"Added Occupancy(id:{new_occupancy.id}, sessionId:{session_id}, weekdayId:{weekdays[day]}, timeSlotId:{time_slot_id}, studentCount:{course.enrollment_total})"
+                            )
+                            occupancy_map[
+                                f"{int(course.crse_id)}{int(course.strm)}{int(course.class_section)}{weekdays[day]}{timeslots_map[time]}"
+                            ] = new_occupancy.id
+                        except IntegrityError:
+                            logging.info(
+                                f"Occupancy(id:{new_occupancy.id}, sessionId:{session_id}, weekdayId:{weekdays[day]}, timeSlotId:{timeslots_map[time]}, studentCount:{course.enrollment_total}) already exists. Skipping."
+                            )
+                            session.rollback()
+                    else:
+                        logging.info(
+                            f"Occupancy(sessionId:{sessions_map[f'{int(course.crse_id)}{int(course.strm)}{int(course.class_section)}']}) already exists. Skipping."
+                        )
 
     session.commit()
     session.close()
--- supabase_models.py
+++ supabase_models.py
@@ -1,140 +1,165 @@
-import datetime
 from typing import List, Optional
 
-from sqlalchemy import (
-    Enum,
-    ForeignKeyConstraint,
-    Integer,
-    PrimaryKeyConstraint,
-    Text,
-    Time,
-)
+from sqlalchemy import BigInteger, Boolean, Column, DateTime, Double, Enum, ForeignKeyConstraint, Integer, Numeric, PrimaryKeyConstraint, Table, Text, Time
+from sqlalchemy.dialects.postgresql import OID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-import enum
-
-
-class instructionModeEnum(enum.Enum):
-    virtual = "virtual"
-    inperson = "inperson"
+import datetime
 
 class Base(DeclarativeBase):
     pass
 
 
 class Major(Base):
-    __tablename__ = "Major"
-    __table_args__ = (PrimaryKeyConstraint("id", name="Major_pkey"),)
+    __tablename__ = 'Major'
+    __table_args__ = (
+        PrimaryKeyConstraint('id', name='Major_pkey'),
+    )
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     name: Mapped[Optional[str]] = mapped_column(Text)
 
-    Course: Mapped[List["Course"]] = relationship("Course", back_populates="Major_")
+    Course: Mapped[List['Course']] = relationship('Course', back_populates='Major_')
 
 
 class Term(Base):
-    __tablename__ = "Term"
-    __table_args__ = (PrimaryKeyConstraint("id", name="Term_pkey"),)
+    __tablename__ = 'Term'
+    __table_args__ = (
+        PrimaryKeyConstraint('id', name='Term_pkey'),
+    )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[Optional[str]] = mapped_column(Text)
 
-    Session: Mapped[List["Session"]] = relationship("Session", back_populates="Term_")
+    Session: Mapped[List['Session']] = relationship('Session', back_populates='Term_')
 
 
 class TimeSlot(Base):
-    __tablename__ = "TimeSlot"
-    __table_args__ = (PrimaryKeyConstraint("id", name="TimeSlot_pkey"),)
+    __tablename__ = 'TimeSlot'
+    __table_args__ = (
+        PrimaryKeyConstraint('id', name='TimeSlot_pkey'),
+    )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     time: Mapped[Optional[datetime.time]] = mapped_column(Time)
 
-    Occupancy: Mapped[List["Occupancy"]] = relationship(
-        "Occupancy", back_populates="TimeSlot_"
-    )
+    Occupancy: Mapped[List['Occupancy']] = relationship('Occupancy', back_populates='TimeSlot_')
 
 
 class Weekday(Base):
-    __tablename__ = "Weekday"
-    __table_args__ = (PrimaryKeyConstraint("id", name="Weekday_pkey"),)
+    __tablename__ = 'Weekday'
+    __table_args__ = (
+        PrimaryKeyConstraint('id', name='Weekday_pkey'),
+    )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[Optional[str]] = mapped_column(Text)
 
-    Occupancy: Mapped[List["Occupancy"]] = relationship(
-        "Occupancy", back_populates="Weekday_"
-    )
+    Occupancy: Mapped[List['Occupancy']] = relationship('Occupancy', back_populates='Weekday_')
+
+
+t_pg_stat_statements = Table(
+    'pg_stat_statements', Base.metadata,
+    Column('userid', OID),
+    Column('dbid', OID),
+    Column('toplevel', Boolean),
+    Column('queryid', BigInteger),
+    Column('query', Text),
+    Column('plans', BigInteger),
+    Column('total_plan_time', Double(53)),
+    Column('min_plan_time', Double(53)),
+    Column('max_plan_time', Double(53)),
+    Column('mean_plan_time', Double(53)),
+    Column('stddev_plan_time', Double(53)),
+    Column('calls', BigInteger),
+    Column('total_exec_time', Double(53)),
+    Column('min_exec_time', Double(53)),
+    Column('max_exec_time', Double(53)),
+    Column('mean_exec_time', Double(53)),
+    Column('stddev_exec_time', Double(53)),
+    Column('rows', BigInteger),
+    Column('shared_blks_hit', BigInteger),
+    Column('shared_blks_read', BigInteger),
+    Column('shared_blks_dirtied', BigInteger),
+    Column('shared_blks_written', BigInteger),
+    Column('local_blks_hit', BigInteger),
+    Column('local_blks_read', BigInteger),
+    Column('local_blks_dirtied', BigInteger),
+    Column('local_blks_written', BigInteger),
+    Column('temp_blks_read', BigInteger),
+    Column('temp_blks_written', BigInteger),
+    Column('blk_read_time', Double(53)),
+    Column('blk_write_time', Double(53)),
+    Column('temp_blk_read_time', Double(53)),
+    Column('temp_blk_write_time', Double(53)),
+    Column('wal_records', BigInteger),
+    Column('wal_fpi', BigInteger),
+    Column('wal_bytes', Numeric),
+    Column('jit_functions', BigInteger),
+    Column('jit_generation_time', Double(53)),
+    Column('jit_inlining_count', BigInteger),
+    Column('jit_inlining_time', Double(53)),
+    Column('jit_optimization_count', BigInteger),
+    Column('jit_optimization_time', Double(53)),
+    Column('jit_emission_count', BigInteger),
+    Column('jit_emission_time', Double(53))
+)
+
+
+t_pg_stat_statements_info = Table(
+    'pg_stat_statements_info', Base.metadata,
+    Column('dealloc', BigInteger),
+    Column('stats_reset', DateTime(True))
+)
 
 
 class Course(Base):
-    __tablename__ = "Course"
+    __tablename__ = 'Course'
     __table_args__ = (
-        ForeignKeyConstraint(
-            ["majorId"], ["Major.id"], name="Course_majorId_Major_id_fk"
-        ),
-        PrimaryKeyConstraint("id", name="Course_pkey"),
+        ForeignKeyConstraint(['majorId'], ['Major.id'], name='Course_majorId_Major_id_fk'),
+        PrimaryKeyConstraint('id', name='Course_pkey')
     )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     majorId: Mapped[Optional[int]] = mapped_column(Integer)
 
-    Major_: Mapped[Optional["Major"]] = relationship("Major", back_populates="Course")
-    Session: Mapped[List["Session"]] = relationship("Session", back_populates="Course_")
+    Major_: Mapped[Optional['Major']] = relationship('Major', back_populates='Course')
+    Session: Mapped[List['Session']] = relationship('Session', back_populates='Course_')
 
 
 class Session(Base):
-    __tablename__ = "Session"
+    __tablename__ = 'Session'
     __table_args__ = (
-        ForeignKeyConstraint(
-            ["courseId"], ["Course.id"], name="Session_courseId_Course_id_fk"
-        ),
-        ForeignKeyConstraint(["termId"], ["Term.id"], name="Session_termId_Term_id_fk"),
-        PrimaryKeyConstraint("id", name="Session_pkey"),
+        ForeignKeyConstraint(['courseId'], ['Course.id'], name='Session_courseId_Course_id_fk'),
+        ForeignKeyConstraint(['termId'], ['Term.id'], name='Session_termId_Term_id_fk'),
+        PrimaryKeyConstraint('id', name='Session_pkey')
     )
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     courseId: Mapped[Optional[int]] = mapped_column(Integer)
     termId: Mapped[Optional[int]] = mapped_column(Integer)
     classSection: Mapped[Optional[int]] = mapped_column(Integer)
-    instructionMode: Mapped[Optional[str]] = mapped_column(
-        Enum("virtual", "inperson", name="instructionMode")
-    )
+    instructionMode: Mapped[Optional[str]] = mapped_column(Enum('virtual', 'inperson', name='instructionMode'))
 
-    Course_: Mapped[Optional["Course"]] = relationship(
-        "Course", back_populates="Session"
-    )
-    Term_: Mapped[Optional["Term"]] = relationship("Term", back_populates="Session")
-    Occupancy: Mapped[List["Occupancy"]] = relationship(
-        "Occupancy", back_populates="Session_"
-    )
+    Course_: Mapped[Optional['Course']] = relationship('Course', back_populates='Session')
+    Term_: Mapped[Optional['Term']] = relationship('Term', back_populates='Session')
+    Occupancy: Mapped[List['Occupancy']] = relationship('Occupancy', back_populates='Session_')
 
 
 class Occupancy(Base):
-    __tablename__ = "Occupancy"
+    __tablename__ = 'Occupancy'
     __table_args__ = (
-        ForeignKeyConstraint(
-            ["sessionId"], ["Session.id"], name="Occupancy_sessionId_Session_id_fk"
-        ),
-        ForeignKeyConstraint(
-            ["timeSlotId"], ["TimeSlot.id"], name="Occupancy_timeSlotId_TimeSlot_id_fk"
-        ),
-        ForeignKeyConstraint(
-            ["weekdayId"], ["Weekday.id"], name="Occupancy_weekdayId_Weekday_id_fk"
-        ),
-        PrimaryKeyConstraint("id", name="Occupancy_pkey"),
+        ForeignKeyConstraint(['sessionId'], ['Session.id'], name='Occupancy_sessionId_Session_id_fk'),
+        ForeignKeyConstraint(['timeSlotId'], ['TimeSlot.id'], name='Occupancy_timeSlotId_TimeSlot_id_fk'),
+        ForeignKeyConstraint(['weekdayId'], ['Weekday.id'], name='Occupancy_weekdayId_Weekday_id_fk'),
+        PrimaryKeyConstraint('id', name='Occupancy_pkey')
     )
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     sessionId: Mapped[Optional[int]] = mapped_column(Integer)
     weekdayId: Mapped[Optional[int]] = mapped_column(Integer)
     timeSlotId: Mapped[Optional[int]] = mapped_column(Integer)
     studentCount: Mapped[Optional[int]] = mapped_column(Integer)
 
-    Session_: Mapped[Optional["Session"]] = relationship(
-        "Session", back_populates="Occupancy"
-    )
-    TimeSlot_: Mapped[Optional["TimeSlot"]] = relationship(
-        "TimeSlot", back_populates="Occupancy"
-    )
-    Weekday_: Mapped[Optional["Weekday"]] = relationship(
-        "Weekday", back_populates="Occupancy"
-    )
+    Session_: Mapped[Optional['Session']] = relationship('Session', back_populates='Occupancy')
+    TimeSlot_: Mapped[Optional['TimeSlot']] = relationship('TimeSlot', back_populates='Occupancy')
+    Weekday_: Mapped[Optional['Weekday']] = relationship('Weekday', back_populates='Occupancy')
